f9dbfa51c7101bf4215c2ba7c82f989a970d8323
Денис Лариков <serzhantov-02@mail.ru>
2022-07-08 14:37:24 +0500

    main_button: redid the accepted values in Button and changed the way of styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,18 +2,29 @@ import React, { CSSProperties } from "react";
 import { FC } from "react";
 import styles from "./Button.module.scss"
 
-interface IButton {
-    label: string;
+interface ButtonProps {
+    color?: string;
+    children?: React.ReactNode;
+    height?: string | number;
+    onClick?: (e: React.FormEvent<HTMLButtonElement>) => void;
+    width?: string | number;
     img?: string;
     style?: CSSProperties;
-    onClick?: (e: React.FormEvent<HTMLButtonElement>) => void;
 }
 
-const Button:FC<IButton> = ({label, img, style, onClick}) => {
+const Button:FC<ButtonProps> = ({
+    color,
+    children,
+    height,
+    onClick,
+    width,
+    style,
+    img
+}) => {
     return (
-        <button onClick={onClick} style={style} className={styles.button}>
+        <button onClick={onClick} style={{backgroundColor: color, height, width, ...style}} className={styles.button}>
             {img && <img src={img} alt=""/>}
-            {label}
+            {children}
         </button>
     )
 }
