b04503efff08702dea409d3aef5b8a1c79626b23
Денис Лариков <serzhantov-02@mail.ru>
2022-07-14 13:18:51 +0500

    refs_on_exam: I made a refs on exam page

--- src/views/mainPage/MainBanner/MainBanner.tsx
+++ src/views/mainPage/MainBanner/MainBanner.tsx
@@ -2,7 +2,6 @@ import Button from "../../../components/Button/Button";
 import Container from "../../../components/Container/Container";
 import styles from "./MainBanner.module.scss"
 import pddHunt2 from "../../../assets/pddHunt2.png"
-import { Link } from "react-router-dom";
 import {getRandomArbitrary} from "../../../utils/utils";
 const MainBanner = () => {
     return (
@@ -17,7 +16,7 @@ const MainBanner = () => {
                             Покажи, что ты можешь, и реши экзамен без единой ошибки
                         </h3>
                         <div className={styles.banner__centering}>
-                            <Link to={"/exam/" + getRandomArbitrary(1, 41)}><Button color="red" width={250}>Пройти экзамен</Button></Link>
+                            <Button color="red" width={250} to={"/exam/" + getRandomArbitrary(1, 41)}>Пройти экзамен</Button>
                         </div>
                     </div>
                     <div className={styles.banner__picture}>
